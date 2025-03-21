7cc77c770335498f97032e03cd677b7ff24b97db
palakSingh621 <kushwahps621@gmail.com>
2025-03-21 16:59:32 +0530

    [Palak] Add: UC5 Input Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
   userName: string="Palak";
+  nameError: string="Error";
   
   ngOnInit():void {
     this.title ="Hello from BridgeLabz";
@@ -17,5 +18,15 @@ export class AppComponent {
   onClick($event){
     console.log("Save button is clicked!", $event);
     window.open(this.url,"_blank");
+  }
+  onInput($event){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex=RegExp('[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName))
+    {
+      this.nameError="";
+      return ;
+    }
+    this.nameError="Name is Incorrect!";
   }
 }
