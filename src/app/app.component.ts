8f1173261b6e72617d35d5de8092444712298732
palakSingh621 <kushwahps621@gmail.com>
2025-03-21 16:18:34 +0530

    [Palak] Add: Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  
   ngOnInit():void {
     this.title ="Hello from BridgeLabz";
+  }
+  onClick($event){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url,"_blank");
   }
 }
